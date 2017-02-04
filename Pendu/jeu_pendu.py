06b1552149a37add99363992dbf31d127813c3bc
Fabien Lavoie <lavoie.fabien@courrier.uqam.ca>
2017-02-04 13:26:22 -0500

    ajout d'un fichier de mot

---
+++ Pendu/jeu_pendu.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+class Pendu():
+    lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZÀÈÂÊÔÛÉËÏÇ"
+
+
+
+
+
+if __name__ == "__main__":
+    mon_pendu = Pendu()
+    print(mon_pendu.lettres)
